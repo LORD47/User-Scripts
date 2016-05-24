304d61eaf893065a23cb92169127cc47f968eca2
LORD47 <LORD47@users.noreply.github.com>
2016-05-24 14:19:53 +0100

    Update Kat URL decoder.user.js

--- Kat URL decoder.user.js
+++ Kat URL decoder.user.js
@@ -14,36 +14,30 @@ var $j = jQuery.noConflict(true);
 
 // expad
 function replaceURl(aLink)
-{    // deprecated: https://kat.cr/confirm/url/aHR0cDovL2ltZ2NsaWNrLm5ldC81eXRqNmMyaGxmYW0vdmxjc25hcC0yMDE1LTA2LTExLTE2aDQ0bTU1czMzLnBuZy5odG1s/
-    //https://ext.kat.cr/confirm/url/aHR0cDovL2ltZzI0Lm9yZy85NGl1Lmh0bWw/
+{    //https://kat.cr/confirm/url/aHR0cDovL2ltZ2NsaWNrLm5ldC81eXRqNmMyaGxmYW0vdmxjc25hcC0yMDE1LTA2LTExLTE2aDQ0bTU1czMzLnBuZy5odG1s/
  var  URL = $j.trim(aLink.attr('href'));
+    var regExp = /^(?:https?:\/\/)?[^/]*\/confirm\/url\/([A-Za-z0-9+/]+)[^/]*\/.*$/i;
 
-   // var regExp = /^(?:https?:\/\/)?[^/]*\/confirm\/url\/([A-Za-z0-9+/]+)[^/]*\/.*$/i;
-   //var regExp = /^(?:https?:\/\/)?[^/]*\/confirm\/url\/([A-Za-z0-9+/]{4})*([A-Za-z0-9+/]{4}|[A-Za-z0-9+/]{3}=|[A-Za-z0-9+/]{2}==).*$/i;
+    //var regExp = /^(?:https?:\/\/)?[^/]*\/confirm\/url\/([A-Za-z0-9+/]{4})*([A-Za-z0-9+/]{4}|[A-Za-z0-9+/]{3}=|[A-Za-z0-9+/]{2}==).*$/i;
 
   //var  encodedBase64 = URL.replace(/^(?:https?:\/\/)?[^/]*\/confirm\/url\/([^/]+).*$/i, '$1');
 
-    //var encodedBase64 = URL.replace(regExp, '$1');
-    var encodedBase64 = aLink.text();
+    var encodedBase64 = URL.replace(regExp, '$1');
 
       if(encodedBase64)
       {
-        //aLink.attr('href', window.atob(encodedBase64));
-        aLink.attr('href', encodedBase64);
+        aLink.attr('href', window.atob(encodedBase64));
         aLink.css('color', 'red');
       }
-
 }
-
 
 
 (function () {
     //$j(document).ready(function(){
     $j(document).ready(function(){
 
-        $j('a[href^="//ext.kat.cr/confirm/url/"]').each(function(){replaceURl( $j(this) ); });
+        $j('a[href^="/confirm/url/"]').each(function(){ replaceURl( $j(this) ); });
 
     });
 
 })();
-
